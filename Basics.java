43b7fc6215d8b4422ec5b8f13123363f0d9b5f2a
Pardeshi2309 <pardeshijaiswar.149@gmail.com>
2024-05-05 11:50:29 +0530

    added Basics Java file

---
+++ Basics.java
@@ -0,0 +1,17 @@
+public class Basics {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a); // 10
+        System.out.println(b); // 5
+
+        String name = "Pardeshi";
+        System.out.println(name); // Pardeshi
+
+        a = 50;
+        System.out.println(a); // 55
+
+    
+    }
+    
+}
