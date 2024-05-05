46a296931816df2596362dcf0c61eb79b73a2781
Pardeshi2309 <pardeshijaiswar.149@gmail.com>
2024-05-05 12:00:35 +0530

    sum files

---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+    public static void main(String args[]) {
+        int a = 5; //a=5 Single Comment 
+        int b = 10;/*Multiple Line Comment */
+        int c = a+b;
+        System.out.println(c);
+    }
+}
